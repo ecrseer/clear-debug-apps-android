acb901e152f61383861c7a3ec03e7887f668ab7a
ecrseer <juustino@live.com>
2022-01-18 20:22:16 -0300

    view model escaneia por apps debug

--- app/src/main/java/br/ecrseer/cleardebugapps/ui/main/MainViewModel.kt
+++ app/src/main/java/br/ecrseer/cleardebugapps/ui/main/MainViewModel.kt
@@ -2,13 +2,35 @@ package br.ecrseer.cleardebugapps.ui.main
 
 import android.content.Intent
 import android.content.pm.ApplicationInfo
+import android.content.pm.ResolveInfo
 import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    val appsParaSeremDeletados = MutableLiveData<MutableList<ApplicationInfo>>()
+    val todosOsApps = MutableLiveData<MutableList<ApplicationInfo>>()
+    fun buscaPorAppsDebug(resolveInfoList:List<ResolveInfo>){
+        try {
+            val apps= mutableListOf<ApplicationInfo>()
+            val intent = Intent(Intent.ACTION_MAIN, null)
+            intent.addCategory(Intent.CATEGORY_LAUNCHER)
 
+            for (resolveInfo in resolveInfoList) {
+                val flags = resolveInfo.activityInfo.applicationInfo.flags
+                val isDebuggable = 0 != flags and ApplicationInfo.FLAG_DEBUGGABLE
+                if(isDebuggable){
+                    apps.add(resolveInfo.activityInfo.applicationInfo)
+                }
+
+            }
+            todosOsApps.postValue(apps)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
 
 
