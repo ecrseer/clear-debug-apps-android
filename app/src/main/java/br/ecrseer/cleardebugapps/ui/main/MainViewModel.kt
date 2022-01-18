0dd511345e81ed8d1c5bca58d4118472050d8c49
ecrseer <juustino@live.com>
2022-01-18 20:18:15 -0300

    imports

---
+++ app/src/main/java/br/ecrseer/cleardebugapps/ui/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package br.ecrseer.cleardebugapps.ui.main
+
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.net.Uri
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+
+
+
+
+}
